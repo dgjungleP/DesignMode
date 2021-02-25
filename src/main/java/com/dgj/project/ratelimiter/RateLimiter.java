17a67849b8e9c8aa16566d688f6f83674be51994
dengguijiang <dgjungle_c@163.com>
2021-02-25 17:17:03 +0800

    初始化限流框架骨架

---
+++ src/main/java/com/dgj/project/ratelimiter/RateLimiter.java
@@ -0,0 +1,9 @@
+package com.dgj.project.ratelimiter;
+
+/**
+ * @version: v1.0
+ * @date: 2021/2/25
+ * @author: dgj
+ */
+public class RateLimiter {
+}
---
+++ src/main/java/com/dgj/project/ratelimiter/alg/RateLimitAlg.java
@@ -0,0 +1,9 @@
+package com.dgj.project.ratelimiter.alg;
+
+/**
+ * @version: v1.0
+ * @date: 2021/2/25
+ * @author: dgj
+ */
+public class RateLimitAlg {
+}
